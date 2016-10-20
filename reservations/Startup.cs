2c710a99ba5f0069e67c039bf0a24b333f76966a
cpatel22 <er.patelchirag@gmail.com>
2016-10-20 19:57:40 -0400

    aaa

---
+++ reservations/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(reservations.Startup))]
+namespace reservations
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
