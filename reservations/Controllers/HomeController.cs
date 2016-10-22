2d90c62f75b5c23a5425ab05773fff2a14e9e741
cpatel22 <er.patelchirag@gmail.com>
2016-10-22 17:13:23 -0400

    Final code

--- reservations/Controllers/HomeController.cs
+++ reservations/Controllers/HomeController.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Reservation;
 
 namespace reservations.Controllers
 {
+     
     public class HomeController : Controller
     {
+        BusinessHelper bhelper = new BusinessHelper();
+
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult Index2()
+        {
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            //http://jsfiddle.net/pxfunc/j3AN7/
             return View();
         }
 
@@ -25,6 +32,28 @@ namespace reservations.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+       
+        public JsonResult SaveOpportunitie(string strmsg)
+        {  
+            Response response = new Response();
+            try
+            {
+                response = bhelper.Transaction_Opportunities(strmsg);
+                if (response.ErrResponse != null || response.ErrResponse == "")
+                {
+                    throw new Exception(response.ErrResponse);
+                }
+
+                return Json(new { result = true, message = "", status = bhelper.CreateHTMLmessage(response) }, JsonRequestBehavior.AllowGet);
+            }
+            catch(Exception ex)
+            {
+                response.ResponseCode = ResCode.Error;
+                response.ErrResponse = ex.Message.ToString();
+
+                return Json(new { result = true, message = "", status = bhelper.CreateHTMLmessage(response) }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
---
+++ reservations/DBFactory/DBFactory.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Reservation.DBFactory
+{
+
+    public class DBFactory
+    {
+        private string _connectionstring = "";
+        private DbConnection _connection;
+        private DbCommand _command;
+        private DbProviderFactory _factory = null;
+
+
+        public string connectionstring
+        {
+            get
+            {
+                return _connectionstring;
+            }
+            set
+            {
+                if (value != "")
+                {
+                    _connectionstring = value;
+                }
+            }
+        }
+
+
+        public DbConnection connection
+        {
+            get
+            {
+                return _connection;
+            }
+        }
+
+
+        public DbCommand command
+        {
+            get
+            {
+                return _command;
+            }
+        }
+
+
+
+
+        public void CreateDBObjects(string connectString, Providers providerList)
+        {
+            switch (providerList)
+            {
+                case Providers.SqlServer:
+                    _factory = SqlClientFactory.Instance;
+                    break;
+
+
+            }
+            _connection = _factory.CreateConnection();
+            _command = _factory.CreateCommand();
+            _connection.ConnectionString = connectString;
+            _command.Connection = connection;
+        }
+
+
+        public int AddParameter(string name, object value)
+        {
+            DbParameter parm = _factory.CreateParameter();
+            parm.ParameterName = name;
+            parm.Value = value;
+            return command.Parameters.Add(parm);
+        }
+
+
+        public int AddParameter(DbParameter parameter)
+        {
+            return command.Parameters.Add(parameter);
+        }
+
+
+        private void BeginTransaction()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            command.Transaction = connection.BeginTransaction();
+        }
+
+
+        private void CommitTransaction()
+        {
+            command.Transaction.Commit();
+            connection.Close();
+        }
+
+
+        private void RollbackTransaction()
+        {
+            command.Transaction.Rollback();
+            connection.Close();
+        }
+
+
+        public int ExecuteNonQuery(string query, CommandType commandtype, ConnectionState connectionstate)
+        {
+            command.CommandText = query;
+            command.CommandType = commandtype;
+            int i = -1;
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                BeginTransaction();
+                i = command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                RollbackTransaction();
+                throw (ex);
+            }
+            finally
+            {
+                CommitTransaction();
+                command.Parameters.Clear();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+            return i;
+        }
+
+
+        public object ExecuteScaler(string query, CommandType commandtype, ConnectionState connectionstate)
+        {
+            command.CommandText = query;
+            command.CommandType = commandtype;
+            object obj = null;
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                BeginTransaction();
+                obj = command.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                RollbackTransaction();
+                throw (ex);
+            }
+            finally
+            {
+                CommitTransaction();
+                command.Parameters.Clear();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                    command.Dispose();
+                }
+            }
+            return obj;
+        }
+
+
+        public DbDataReader ExecuteReader(string query, CommandType commandtype, ConnectionState connectionstate)
+        {
+            command.CommandText = query;
+            command.CommandType = commandtype;
+            DbDataReader reader = null;
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                if (connectionstate == System.Data.ConnectionState.Open)
+                {
+                    reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                }
+                else
+                {
+                    reader = command.ExecuteReader();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+            finally
+            {
+                command.Parameters.Clear();
+            }
+            return reader;
+        }
+
+
+        public DataSet GetDataSet(string query, CommandType commandtype, ConnectionState connectionstate)
+        {
+            DbDataAdapter adapter = _factory.CreateDataAdapter();
+            command.CommandText = query;
+            command.CommandType = commandtype;
+            adapter.SelectCommand = command;
+            DataSet ds = new DataSet();
+            try
+            {
+                adapter.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                command.Parameters.Clear();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                    command.Dispose();
+                }
+            }
+            return ds;
+        }
+
+
+        public enum Providers
+        {
+            SqlServer
+
+        }
+    }
+}
